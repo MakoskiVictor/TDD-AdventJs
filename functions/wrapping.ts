365f3b64cee727642550a92e018d7b851d5041e6
MakoskiVictor <makoski.ed@gmail.com>
2022-12-07 21:02:01 -0300

    restructuring wrapping

--- functions/wrapping.ts
+++ functions/wrapping.ts
@@ -1,4 +1,4 @@
-export function wrapping(gifts /* : Array<string> */): Array<string> {
+/* export function wrapping(gifts  : Array<string> ): Array<string> {
     if (Array.isArray(gifts) === false)
       throw new Error('The param should be an array');
     if (gifts.some(e => typeof e !== 'string'))
@@ -13,4 +13,15 @@ export function wrapping(gifts /* : Array<string> */): Array<string> {
       gift.push(wrapped);
     }
     return gift;
+  }  */
+
+  //RESTRUCTURED
+
+  export function wrapping(gifts /* : Array<string> */): Array<string> {
+    if (Array.isArray(gifts) === false)
+      throw new Error('The param should be an array');
+    if (gifts.some(e => typeof e !== 'string'))
+      throw new Error('The content of array should be an string');
+      let wrap = "*"
+      return gifts.map(g => `${wrap.repeat(g.length+2)}\n*${g}*\n${wrap.repeat(g.length+2)}`)
   }
